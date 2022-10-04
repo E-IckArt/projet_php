ad691a051846ac8e5e2f2c78f8ab2c4b282a3d6a
Emilie Ick-Art <emilie.inack-audibert@le-campus-numerique.fr>
2022-10-04 12:01:04 +0200

    Add home.php

--- index.php
+++ index.php
@@ -3,20 +3,13 @@
 $title = "Mon Super site - Accueil";
 
 include_once './layouts/header.php';
+
+include_once './layouts/home.php';
+
+include_once './layouts/footer.php';
+
 ?>
 
-<main class="container">
-    <div class="row">
-        <div class="col-12 col-md-10 col-lg-8 mx-auto text-center">
-            <h1>Bienvenue sur le site</h1>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-12 col-md-10 col-lg-8 mx-auto">
-            PAGE EN CONSTRUCTION
-        </div>
-    </div>
-</main>
 
-<?php include_once './layouts/footer.php'; ?>
+
 
---
+++ layouts/home.php
@@ -0,0 +1,19 @@
+<main class="container">
+    <div class="row">
+        <div class="col-12 col-md-10 col-lg-8 mx-auto text-center">
+            <h1>Bienvenue sur le site Diététique Heyrieux</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12 col-md-10 col-lg-8 mx-auto text-center">
+            <div class="fs-1 text-center fw-bold">
+            <p class="my-5">
+            PAGE EN CONSTRUCTION
+            </p>
+            <img src="../assets/home.svg" alt="#">
+            </div>
+
+
+        </div>
+    </div>
+</main>
