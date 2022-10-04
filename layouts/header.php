77a5a5f9199d13373fd6036f032d83dc1d97f1d6
Emilie Ick-Art <emilie.inack-audibert@le-campus-numerique.fr>
2022-10-04 11:11:53 +0200

    Ft - Add  fontawesome links

--- layouts/header.php
+++ layouts/header.php
@@ -10,10 +10,18 @@
         <link rel="shortcut icon" href="../assets/favicon.png" type="image/png">
         <!-- Bootstrap CSS only -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        <!-- Font Awesome -->
+        <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+            integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+            crossorigin="anonymous"
+            referrerpolicy="no-referrer"
+        />
     </head>
 
     <body class="d-flex flex-column min-vh-100 bg-dark">
-        <header class="mb-5">
+        <header id="header" class="mb-5">
             <nav class="navbar navbar-expand-lg bg-light">
                 <div class="container-fluid">
                 <a class="navbar-brand" href="#">
