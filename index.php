b9d2c3f04ff157045354323102d12d92ec87ead4
Emilie Ick-Art <emilie.inack-audibert@le-campus-numerique.fr>
2022-10-04 11:26:18 +0200

    Ft - Add main template

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+$title = "Mon Super site - Accueil";
+
+include_once './layouts/header.php';
+?>
+
+<main class="container">
+    <div class="row">
+        <div class="col-12 col-md-10 col-lg-8 mx-auto text-center">
+            <h1>Bienvenue sur le site</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12 col-md-10 col-lg-8 mx-auto">
+            PAGE EN CONSTRUCTION
+        </div>
+    </div>
+</main>
+
+<?php include_once './layouts/footer.php'; ?>
+
